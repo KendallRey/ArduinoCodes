88cbeb5b0cf4732d1547dc9a271b23921035be01
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-08-09 23:19:19 +0800

    update servo leg class

--- 2JointLeg_servo_nrfl01/2JointLeg_servo_nrfl01.ino
+++ 2JointLeg_servo_nrfl01/2JointLeg_servo_nrfl01.ino
@@ -20,108 +20,134 @@ int intReadings[3] = {0,0,0};
 class ServoLeg {
   private:
     Servo mainServo; 
-    int center;
-    int delaySpeed = 1;
+    int center = 90;
+    int delaySpeed = 10;
   
     int pos = 0;
     unsigned long previousMillis = 0;
     int increment = 1;
 
+    int max = 180;
+    int min = 0;
+
   public:
 
-    // Constructor
-    ServoLeg(int _servoPin, int _centerValue, int _delaySpeed) {
-      mainServo.attach(_servoPin);
-      center = _centerValue;
-      delaySpeed = _delaySpeed;
+    ServoLeg() {
+      pos = 90;
     }
 
-    void move(int speed) {
+    void init(int _servoPin, int _centerValue) {
+      mainServo.attach(_servoPin);
+      center = _centerValue;
+      pos = _centerValue;
+    }
+  
+    void init(int _servoPin, int _centerValue, bool reverseStart) {
+      mainServo.attach(_servoPin);
+      center = _centerValue;
+      pos = _centerValue;
+      if(reverseStart) {
+        increment = -increment;
+      }
+    }
+
+    void init(int _servoPin, int _centerValue, bool reverseStart, int _min, int _max) {
+      mainServo.attach(_servoPin);
+      center = _centerValue;
+      pos = _centerValue;
+      max = _max;
+      min = _min;
+    }
+
+    void moveDelayed(int speed) {
       unsigned long currentMillis = millis();
 
       if (currentMillis - previousMillis >= delaySpeed) {
         // Save the last time you updated the servo position
         previousMillis = currentMillis;
-          if(speed <= 1 && speed >= -1){
-        pos = center;
-      } else {
+        if(speed <= 2 && speed >= -2){
+          pos = center;
+        } else {
 
-        if(speed > 1) {
-          pos += increment;
+          if(speed > 1) {
+            pos += increment;
+          }
+          else if (speed < -1) {
+            pos -= increment;
+          }
+          
+          if (pos >= max || pos <= min) {
+            increment = -increment;
+          }
         }
-        else if (speed < -1) {
-          pos -= increment;
-        }
-        
-        if (pos >= 180 || pos <= 0) {
-          increment = -increment;
-        }
-      }
         mainServo.write(pos);
       }
     }
 
-    void move2(int speed) {
-      if(speed <= 1 && speed >= -1){
-        pos = center;
-      } else {
+    void move(int speed) {
+      if(speed <= 2 && speed >= -2){
+          pos = center;
+        } else {
 
-        if(speed > 1) {
-          pos += increment;
+          if(speed > 1) {
+            pos += increment;
+          }
+          else if (speed < -1) {
+            pos -= increment;
+          }
+          
+          if (pos >= max || pos <= min) {
+            increment = -increment;
+          }
         }
-        else if (speed < -1) {
-          pos -= increment;
-        }
-        
-        if (pos >= 180 || pos <= 0) {
-          increment = -increment;
-        }
-      }
-      Serial.println(pos);
       mainServo.write(pos);
     }
 };
 
-// ServoLeg legFL(6, 90, 1);
+ServoLeg legFL;
+ServoLeg legFR;
 
-
-int increment = 1;
-int center = 90;
-int pos = 0;
 unsigned long previousMillis = 0;
 int delaySpeed = 10; 
 
-
-//create servo object
-Servo servoX;
-Servo servoY;
-const int servoXPin = 5;
-const int servoYPin = 6;
-
 void setupServo() {
-  servoX.attach(servoXPin);
-  servoY.attach(servoYPin);
+
+  legFL.init(5, 90, false, 30, 150);
+  legFR.init(6, 90, true, 30, 150);
 }
 
 void setup() {
   Serial.begin(9600);
   setupRadio();
   setupServo();
+  delay(500);
 }
 
 void loop() {
   readData();
 
-
   // Serial.println(potValue);
-  int angleX = map(intReadings[1], -1023, 1023, -20, 20);
   // int val = map(potValue, -1023, 1023, -20, 20);
-  
-  // Serial.println(angleX);
+  moveLegs();
   // servoX.write(angleX);
-  moveServo(angleX);
+  // moveServo(angleX);
   // delay(delaySpeed);
 }
+
+
+void moveLegs() {
+  
+  int angleX = map(intReadings[1], -1023, 1023, -20, 20);
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= delaySpeed) {
+      // Save the last time you updated the servo position
+      previousMillis = currentMillis;
+      legFL.move(angleX);
+      legFR.move(angleX);
+    }
+};
 
 void setupRadio() {
   bool isBegin = radio.begin();
